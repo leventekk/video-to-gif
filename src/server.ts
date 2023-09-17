e597ba111b93f4142c864a06a7e22a97f669e097
levente kiraly <leventekk@users.noreply.github.com>
2023-09-17 17:50:26 +0200

    feat(application): add environment support

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
+import 'dotenv/config';
 import Fastify from 'fastify';
+
 const fastify = Fastify({
   logger: true,
 });
@@ -9,7 +11,7 @@ fastify.get('/healthcheck', async function handler() {
 
 (async () => {
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ port: Number(process.env.PORT) });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
