e9c48598fb7dea477d8430de3e4e9b0e92345509
levente kiraly <leventekk@users.noreply.github.com>
2023-09-19 21:40:19 +0200

    chore(lint): update lint-staged file

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,29 @@
+/**
+ * @callback composeCallback
+ * @param {string[]} files Staged Files
+ * @returns {string} Composed command with optional filenames
+ */
+
+/**
+ * @function composeCommand
+ * @param {string} command Command to call on the staged files
+ * @return {composeCallback} callback
+ */
+function composeCommand(command) {
+  /**
+   * @function executeCommand
+   * @callback composeCallback
+   */
+  return function executeCommand(files) {
+    if (files.length > 10) {
+      return `${command} .`;
+    }
+
+    return `${command} ${files.join(' ')}`;
+  };
+}
+
 module.exports = {
-  '*': 'prettier --write --ignore-unknown',
-  '*.ts': 'eslint . --fix',
+  '*': composeCommand('prettier --write --ignore-unknown'),
+  '*.ts': composeCommand('eslint --fix'),
 };
